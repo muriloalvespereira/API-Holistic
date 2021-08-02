7695ce42fa6173a41c45470fcfc3a5e50cceaa65
Murilo Alves <murilo.udi@gmail.com>
2021-08-02 17:18:46 +0200

    Putting up the load balancer EC2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const config = require('../config/config');
 
 const auth = (req, res, next) => {
     const token_header = req.headers.auth;
-    if(!token_header) return res.status(401).send({ error: 'Token não enviado!' });
+    if(!token_header) return res.status(200).send({ error: 'Token não enviado!' });
 
     jwt.verify(token_header, config.jwt_pass, (err, decoded) => {
         if (err) return res.status(401).send({ error: 'Token inválido!' });

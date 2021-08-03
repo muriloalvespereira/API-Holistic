1aa211c35dc4b1ba72c117969066752bf1b760ba
Murilo Alves <murilo.udi@gmail.com>
2021-08-03 17:50:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(cors({origin:true,credentials: true}));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', 'holistic.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Requested-With, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
     if (req.method === "OPTIONS") {

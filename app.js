dc27150166aee77fa9da0917adca4effa24f397c
Murilo Alves <murilo.udi@gmail.com>
2022-04-08 15:07:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,19 +28,19 @@ mongoose.connection.on('connected', () => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors({origin:true,credentials: true}));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'https://holistic.vercel.app');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Requested-With, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    } else {
-        next();
-    }
-});
+app.use(cors());
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', 'https://holistic.vercel.app');
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Requested-With, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+//     if (req.method === "OPTIONS") {
+//         return res.status(200).end();
+//     } else {
+//         next();
+//     }
+// });
 
 const indexRoute = require('./Routes/index');
 const usersRoute = require('./Routes/users');

a4cbf0d67c98fd477566dd21f5d61d3ea4ec178f
Rafael Lima <rafauxdev@gmail.com>
2022-06-26 16:23:41 +0100

    Update userModel

--- db/model/user.js
+++ db/model/user.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
+
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema(
@@ -26,6 +27,10 @@ const UserSchema = new Schema(
         whatssapp: { type: String },
         telegram: { type: String },
         email_2: { type: String },
+        acc_confirmed: { type: Boolean, default: false },
+        authToken: { type: String },
+        acc_validation_token: { type: String },
+        acc_validation_token_expires: { type: Date },
         schoolId: { type: Schema.Types.ObjectId, ref: 'School' }
     },
     {
@@ -38,7 +43,20 @@ UserSchema.pre('save', async function (next) {
     if (!user.isModified('password')) return next()
 
     user.password = await bcrypt.hash(user.password, 10)
+
     return next()
 })
+
+UserSchema.methods.toJSON = function () {
+    const userDocument = this
+    const userObject = userDocument.toObject()
+
+    delete userObject.password
+    delete userObject.__v
+    delete userObject.acc_validation_token
+    delete userObject.acc_validation_token_expires
+
+    return userObject
+}
 
 export default mongoose.model('User', UserSchema, 'test')

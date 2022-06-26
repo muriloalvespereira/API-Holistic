4a949e186e3be2b997c6eaa5f8b1ffe8832763a4
Rafael Lima <rafauxdev@gmail.com>
2022-06-26 07:32:55 +0100

    Update school schema

--- db/model/schools.js
+++ db/model/schools.js
@@ -1,15 +1,30 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema(
+const amenitiesList = [
+    'amenity_1',
+    'amenity_2',
+    'amenity_3',
+    'amenity_4',
+    'amenity_5'
+]
+
+const Schools = new Schema(
     {
         title: { type: String, required: true },
         city: { type: String, required: true },
-        country: { type: String, required: true }
+        country: { type: String, required: true },
+        website: { type: String },
+        email: { type: String, lowercase: true },
+        whatssapp: { type: String },
+        telegram: { type: String },
+        description: { type: String },
+        ratting: { type: Number, min: 0, max: 5 },
+        amenities: { type: Array, enum: amenitiesList }
     },
     {
         timestamps: true
     }
 )
 
-export default mongoose.model('School', UserSchema, 'allschools')
+export default mongoose.model('School', Schools, 'allschools')

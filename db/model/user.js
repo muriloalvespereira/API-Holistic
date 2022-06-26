26b22a44b554dac700664f6ca1eb6f4b382c10e5
Rafael Lima <rafauxdev@gmail.com>
2022-06-26 07:11:25 +0100

    Update user schema

--- db/model/user.js
+++ db/model/user.js
@@ -5,7 +5,24 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema(
     {
         email: { type: String, required: true, unique: true, lowercase: true },
-        password: { type: String, select: false }
+        password: { type: String, select: false, required: true },
+        accountType: {
+            type: String,
+            enum: ['user', 'admin', 'school'],
+            default: 'user',
+            required: true
+        },
+        phone_1: { type: String, required: true },
+        phone_2: { type: String },
+        whatssapp: { type: String },
+        telegram: { type: String },
+        email_2: { type: String },
+        cookies: {
+            type: String,
+            enum: ['essential', 'all'],
+            default: 'essential',
+            required: true
+        }
     },
     {
         timestamps: true

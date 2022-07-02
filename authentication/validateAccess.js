fbea2fba1be135c402bb4ee949711c6bcda928f1
Rafael Lima <rafauxdev@gmail.com>
2022-07-02 10:31:53 +0100

    Create verifyAuthorizationToken and validateAccess

---
+++ authentication/validateAccess.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken'
+
+const { verify } = jwt
+
+const verifyAuthorizationToken = (token) => {
+    try {
+        const res = verify(token, process.env.JWT_SECRET_KEY)
+
+        return res
+    } catch (error) {
+        return error.name
+    }
+}
+
+const validateAccess = async (req, res, next) => {
+    try {
+        if (!req.cookies?.Authentication) {
+            return res
+                .status(401)
+                .send({ success: false, error: 'Token missing' })
+        }
+
+        const authToken = req.cookies.Authentication.split(' ')[1]
+        const tokenData = verifyAuthorizationToken(authToken)
+        if (tokenData?.id) {
+            req.acc_id = tokenData.id
+            next()
+        } else {
+            return res.status(400).send({
+                success: false,
+                msg: 'Token inválido!'
+            })
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+export default validateAccess
